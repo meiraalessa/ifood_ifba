8cdd5517e3886320c3cfefce161531a96d99b7df
deividphp <132299460+deividphp@users.noreply.github.com>
2023-05-26 13:23:52 -0300

    Create dadospizza.php

---
+++ ifood/dadospizza.php
@@ -0,0 +1,79 @@
+<?php
+
+$pizzas = [   
+    [        
+        "nome" => "Pizza Calabresa",
+        "foto" => "imagens/imagen4.jpg",
+        "preco" => 35,        
+        "sobre" => "pizza calabresa é composta por uma deliciosa combinação de ingredientes: uma base de massa de pizza artesanal, 
+        um molho de tomate caseiro, generosas fatias de queijo mussarela de alta qualidade, calabresa defumada e temperada
+       cuidadosamente, além de cebola caramelizada para adicionar um toque de sabor e sofisticação." ,
+       "copy" => "Deliciosa pizza calabresa: Uma combinação perfeita de massa crocante, 
+       molho de tomate, queijo derretido e generosas fatias de calabresa defumada. Sabor irresistível em cada mordida. Voçê amante da pizza calabresa, peça já a sua!"
+       
+    ],
+    [       
+        "nome" => "Pizza Mussarela",
+        "foto" => "imagens/imagen5.jpg",
+        "preco" => 35, 
+        "sobre" => "A pizza de mussarela é feita com uma massa de pizza perfeitamente assada, um molho de tomate rico e saboroso, uma
+        camada abundante de queijo mussarela de qualidade superior, além de um toque de azeite de oliva e folhas frescas de
+         manjericão para realçar ainda mais o seu sabor clássico e sofisticado."    ,
+         "copy" => "Pizza de mussarela irresistível: Massa macia e fina, coberta com o autêntico sabor da mussarela derretida.
+          Simplesmente deliciosa e clássica, perfeita para os amantes de queijo. Saboreie essa tentação hoje mesmo!"
+    ],
+    [   "nome" => "Pizza Portuguesa",
+        "foto" => "imagens/imagen6.jpg",  
+        "preco" => 35,   
+        "sobre" => "A pizza portuguesa é composta por uma massa de pizza deliciosamente crocante, um molho de tomate caseiro, uma combinação 
+        de queijos, presunto fatiado, rodelas de cebola, fatias de ovo cozido e azeitonas pretas. Essa combinação harmoniosa resulta
+         em uma pizza saborosa e tradicional."   ,
+         "copy" => "A clássica pizza portuguesa: Uma combinação perfeita de sabores. Massa crocante, molho de tomate, queijo derretido, presunto, ovos,
+          cebola e azeitonas. Uma explosão de gostos que vai te conquistar. Peça já a sua e se delicie com essa delícia tradicional!"
+    ],
+    [  
+        "nome" => "Pizza Frango",  
+        "foto" => "imagens/imagen7.jpg",
+        "preco" => 35,        
+        "sobre" => "A pizza de frango é preparada com uma massa de pizza perfeitamente assada, um molho de tomate suculento, queijo mussarela 
+        derretido, pedaços suculentos de frango temperado, fatias de cebola e uma seleção de temperos como alho, pimenta e ervas 
+        aromáticas para realçar o sabor do frango. É uma opção deliciosa e satisfatória para os amantes de pizza e frango." ,
+        "copy" => "Pizza de frango irresistível: Massa crocante, molho de tomate, queijo derretido e suculentos pedaços de frango temperado.
+         Experimente essa explosão de sabores em cada pedaço. Peça agora mesmo e deixe-se encantar pela pizza de frango!"
+    ], [  
+        "nome" => "Pizza Palmito",  
+        "foto" => "imagens/palmito.jpg",
+        "preco" => 35,        
+        "sobre" => "Desfrute de uma experiência gastronômica refinada com nossa pizza de palmito. Delicie-se com nossa massa artesanal crocante, cuidadosamente preparada. Sinta o toque irresistível do molho de tomate caseiro, feito com tomates frescos e ervas selecionadas. Descubra a suavidade do palmito fresco, fatiado em pedaços delicados.
+         E para coroar, aprecie a fusão perfeita do queijo mussarela de alta qualidade, derretendo em cada mordida. Uma combinação sofisticada de sabores que encantará seu paladar." ,
+        "copy" => "Pizza de palmito: Uma verdadeira experiência gourmet. Massa leve e crocante, coberta com delicioso molho de tomate, queijo derretido e pedaços suculentos de palmito. 
+        Uma combinação suave e saborosa que vai conquistar seu paladar. Não perca tempo e peça essa delícia de pizza de palmito agora mesmo!"
+    ],
+    [  
+        "nome" => "Pizza Baicon",  
+        "foto" => "imagens/imagen11.jpg",
+        "preco" => 35,        
+        "sobre" => "Desfrute de uma pizza de bacon que eleva o prazer culinário a novos patamares. Delicie-se com nossa massa artesanal crocante e irresistível. Saboreie o equilíbrio perfeito entre o sabor defumado e suculento do bacon cuidadosamente selecionado. Sinta a explosão de sabores ao morder generosas fatias de bacon crocante sobre nosso molho de tomate caseiro. 
+        E para acentuar o deleite, derreta-se com o queijo mussarela de alta qualidade, harmonizando-se perfeitamente com o bacon. Uma experiência irresistível para os amantes do bacon." ,
+        "copy" => "Pizza de bacon irresistível: Massa crocante, coberta com generosas camadas de queijo derretido e pedaços suculentos de bacon crocante e defumado. 
+        Um encontro perfeito de sabores salgados e defumados que vão te deixar com água na boca. Peça já a sua e renda-se ao prazer do bacon na pizza!"
+    ],
+    [  
+        "nome" => "Pizza Peperoni",  
+        "foto" => "imagens/imagen13.jpg",
+        "preco" => 35,        
+        "sobre" => "Deleite-se com uma pizza de pepperoni que desperta os sentidos e encanta o paladar. Desfrute da nossa massa artesanal, crocante e deliciosamente leve. 
+        Saboreie a intensidade do pepperoni fatiado, com seu sabor defumado e levemente picante, harmonizando perfeitamente com o molho de tomate caseiro. Derreta-se com a generosa camada de queijo mussarela de alta qualidade, que se funde perfeitamente com o pepperoni, resultando em uma experiência irresistível. Uma combinação clássica e memorável para os apreciadores de pizza." ,
+        "copy" => "A clássica pizza pepperoni: Uma explosão de sabores intensos. Massa crocante, molho de tomate, queijo derretido e fatias saborosas de pepperoni levemente picante. 
+        Cada mordida é uma festa para o paladar. Satisfaça seus desejos com essa pizza irresistível. Peça agora mesmo e desfrute do sabor marcante do pepperoni!"
+    ],
+    [  
+        "nome" => "Pizza Vegana",  
+        "foto" => "imagens/vegana.jpg",
+        "preco" => 35,        
+        "sobre" => "Delicie-se com uma pizza vegana que celebra os sabores naturais e a diversidade dos ingredientes vegetais. 
+        Desfrute da nossa massa artesanal, leve e crocante, feita sem ingredientes de origem animal. Sinta a explosão de sabores do nosso molho de tomate fresco e temperado, realçado por uma seleção de vegetais frescos e coloridos, como pimentões, cogumelos, cebolas caramelizadas e azeitonas. Finalize com um delicioso queijo vegano derretido, que traz uma textura e um sabor incríveis. Uma pizza vegana cheia de criatividade e sabor que agrada a todos os paladares conscientes." ,
+        "copy" => "Pizza vegana irresistível: Massa crocante, molho de tomate fresco, coberta com uma variedade de legumes frescos e queijo vegano derretido. 
+        Sabores deliciosos, sem ingredientes de origem animal. Uma opção saudável e saborosa. Peça já e surpreenda-se com a pizza vegana!"
+    ]
+];
