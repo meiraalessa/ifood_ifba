bed7ce25d46d301ce53d2e22023eb18f18dccabb
meiraalessa <134645330+meiraalessa@users.noreply.github.com>
2023-05-26 13:05:46 -0300

    Create index.php

---
+++ ifood/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ifood-Inicio</title>
+    <link href="estilo.css" rel="stylesheet">
+ </head>
+ <body>
+    <header><!--header-representa um grupo de suporte introdutório ou navegacional-->
+ <div class="center">
+ <h1>Ifood</h1>
+ <div class="topnav">
+  <a class="active" href="index.php">Inicio</a>
+  <a href="entregadores.php">Entregador</a>
+  <a href="restaurante.php">Restaurante</a>
+  <a href="duvidas.php">Duvidas</a>
+</div>
+</div>
+</header>
+ <main><!--main-define o conteúdo principal dentro do <body>-->
+ <section class="showcase">
+     <div class="overlay"></div>
+    <div class="text">
+    <div class="logo"><img src="imagens/logoletra.png"/></div> <!--Logo-->
+      <h2>Tudo pra facilitar  </h2>
+      <h2>seu dia a dia</h2>
+      <p>O que você precisa está aqui. Peça e receba onde estiver.</p>
+      <a href="top1.php">Explorar</a>
+    </div>
+    <div class="imagem"><img src="imagens/entrega.png"/></div>
+  </section>
+      </main>
+      <footer class="footer">
+  <div class="footer__addr">
+    <h1 class="footer__logo"></h1>
+        
+    <h2>Atendimento ao cliente</h2>
+    
+    <address>
+      (11) 96390-9392<br>
+           <a class="footer__btn" href="suporte@ifood.com.br"> suporte@ifood.com.br
+</a>
+    </address>
+  </div>
+  
+  <ul class="footer__nav">
+    <li class="nav__item">
+      <h2 class="nav__title">  Media</h2>
+
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Em todas as redes sociais</a>
+        </li>
+        <li>
+          <a href="#">@ifoodbrasil</a>
+        </li>
+      </ul>
+    </li>
+    
+    <li class="nav__item nav__item--extra">
+      <h2 class="nav__title">Website Designer by</h2>
+      
+      <ul class="nav__ul nav__ul--extra">
+        <li>
+            <a href="#">Equipe 6</a>
+            </li>
+             <li>
+              <a href="#">Alessa Meira</a>
+           </li>
+          <li>
+             <a href="#">Rebecca Aguiar</a>
+          </li>
+          <li>
+            <a href="#">Edmilson Silva</a>
+          </li>
+          <li>
+            <a href="#">Davi Vieira</a>
+
+        </li>
+      </ul>
+    </li>
+    
+    <li class="nav__item">
+      <h2 class="nav__title">Legal</h2>
+      
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Politica de Privacidade</a>
+        </li>
+        
+        <li>
+          <a href="#">Termos de uso</a>
+        </li>
+        
+      </ul>
+    </li>
+  </ul>
+  
+  <div class="legal">
+    <p>&copy; 2022  All rights reserved.</p>
+    
+    <div class="legal__links">
+      <span>Made with <span class="heart">♥
+    </div>
+  </div>
+</footer>
+ </body>
+ </html>
